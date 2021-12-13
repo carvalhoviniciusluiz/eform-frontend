4d28a45d65b70e5e724b4d15af16d4bd0829c701
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-13 20:48:32 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/app/domain/usecases/authentication.ts
+++ src/app/domain/usecases/authentication.ts
@@ -1,12 +1,12 @@
 import { AccountModel } from '../models'
 
-enum GrantType {
+export enum GrantType {
   PASSWORD_GRANT = 'password_grant',
   REFRESH_TOKEN = 'refresh_token',
   CREATE_CREDENTIALS = 'create_credentials'
 }
 
-type AuthenticationParams = {
+export type AuthenticationParams = {
   grantType: GrantType
   credential: string
   password: string
