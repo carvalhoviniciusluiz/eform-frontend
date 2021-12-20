f6f162171d382f97d12e3fecbfbab3208297f7e2
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-20 09:07:56 -0300

    test: ensure Login with initials start

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Login from './login'
 
 describe('Login component', () => {
+  test('should not render errors on start', () => {
+    render(<Login />)
+    expect(screen.queryByTestId('error-wrap')).toBeNull()
+  })
+
   test('should render continue label on start', () => {
     const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
-    expect(errorWrap.childElementCount).toBe(1)
+    const submit = getByTestId('submit')
+    expect(submit.childElementCount).toBe(1)
+  })
+
+  test('should disabled submit button', () => {
+    const { getByTestId } = render(<Login />)
+    const submit = getByTestId('submit') as HTMLButtonElement
+    expect(submit.disabled).toBe(true)
   })
 })
