02ed059aa90540da5686b8b92d84df4b86b35181
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-21 13:57:30 -0300

    feat: ensure EmailValidation returns falsy if email is invalid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -6,6 +6,8 @@ export class EmailValidation implements FieldValidation {
   constructor(private readonly field: string) {}
 
   validate(value: string): Error {
-    return new InvalidaFieldError()
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(value) ? null : new InvalidaFieldError()
   }
 }
