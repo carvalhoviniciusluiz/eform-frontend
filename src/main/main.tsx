dae2239de88fe223dfdf866bcdcf2b25f5e5bbcc
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-22 15:34:14 -0300

    feat: add Login composition

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { Router } from '@/presentation/components'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <Router makeLogin={makeLogin} />
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,28 +1,15 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Login } from '@/presentation/pages'
 
-const Router = () => {
+type RouterProps = {
+  makeLogin: React.FC
+}
+
+const Router = ({ makeLogin: Login }: RouterProps) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/login'
-          element={
-            <Login
-              validation={{ validate: () => '' }}
-              authentication={{
-                auth: async () => {
-                  return await Promise.resolve({
-                    accessToken: '',
-                    accessTokenExpiresIn: 0,
-                    refreshToken: '',
-                    refreshTokenExpiresIn: 0
-                  })
-                }
-              }}
-            />
-          }
-        ></Route>
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
