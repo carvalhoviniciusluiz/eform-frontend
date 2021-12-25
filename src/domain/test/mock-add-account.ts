9e21cdac17eebe20fb03348abed9c4afdbb7ded1
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-25 14:18:29 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/domain/test/mock-add-account.ts
@@ -0,0 +1,17 @@
+import * as faker from 'faker'
+import { GrantType } from '@/domain'
+import { AddAccountParams } from '../usecases'
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password = faker.internet.password()
+  return {
+    grant_type: GrantType.CREATE_CREDENTIALS,
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    documentNumber: faker.random.alphaNumeric(11),
+    phone: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
