a357804846899146011636a7675fe3e370e683d6
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-25 00:48:53 -0300

    refactor: vite config process env

--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -1,4 +1,4 @@
 export const makeApiUrl = (path: string): string => {
-  const { VITE_BASE_URL } = import.meta.env
+  const { VITE_BASE_URL } = process.env
   return `${VITE_BASE_URL}${path}`
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(resolve(), './src'),
-      "~": resolve(resolve(), './public')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  // https://github.com/vitejs/vite/issues/1149#issuecomment-857686209
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        ['process.env.' + key]: `"${val}"`,
+      }
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: { additionalData: `@import "./public/styles";` },
+    {},
+  )
+
+  return {
+    define: envWithProcessPrefix,
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": resolve(resolve(), './src'),
+        "~": resolve(resolve(), './public')
+      },
     },
+    css: {
+      preprocessorOptions: {
+        scss: { additionalData: `@import "./public/styles";` },
+      },
+    }
   }
 })
