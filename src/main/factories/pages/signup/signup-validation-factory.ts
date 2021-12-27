69b115c7dbb2a22f3556f918586afb507ba40e21
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-27 19:31:56 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,15 @@
+import {
+  ValidationBuilder as Builder,
+  ValidationComposite
+} from '@/validation/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...Builder.field('firstName').min(2).required().build(),
+    ...Builder.field('lastName').min(2).required().build(),
+    ...Builder.field('documentNumber').required().build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('phone').required().build(),
+    ...Builder.field('password').required().min(5).build()
+  ])
+}
