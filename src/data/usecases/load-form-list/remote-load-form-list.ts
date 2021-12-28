a03ff215c3786a0a07ff1ff4380c6284aaf8d067
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2021-12-28 19:46:38 -0300

    feat: ensure RemoteLoadFormList returns a list on 201

--- src/data/usecases/load-form-list/remote-load-form-list.ts
+++ src/data/usecases/load-form-list/remote-load-form-list.ts
@@ -1,17 +1,17 @@
-import { UnexpectedError } from '@/domain'
+import { FormModel, LoadFormList, UnexpectedError } from '@/domain'
 import { HttpGetClient, HttpStatusCode } from '@/data/protocols'
 
-export class RemoteLoadFormList {
+export class RemoteLoadFormList implements LoadFormList {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<FormModel[]>
   ) {}
 
-  async loadAll(): Promise<void> {
+  async loadAll(): Promise<FormModel[]> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body?.data
       default:
         throw new UnexpectedError()
     }
