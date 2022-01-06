9c71f83aa73cfd9bcba93839a029cf611c5efa70
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-01-06 09:50:34 -0300

    test: change order to validadion on the field

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -9,8 +9,8 @@ describe('SignUpValidationFactory', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...Builder.field('firstName').min(2).required().build(),
-        ...Builder.field('lastName').min(2).required().build(),
+        ...Builder.field('firstName').required().min(2).build(),
+        ...Builder.field('lastName').required().min(2).build(),
         ...Builder.field('documentNumber').required().build(),
         ...Builder.field('email').required().email().build(),
         ...Builder.field('phone').required().build(),
