d39203c350e7174f3649e53d1c699c9a03cede94
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-01-10 11:29:02 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/form-list/form-list.spec.tsx
+++ src/presentation/pages/form-list/form-list.spec.tsx
@@ -1,9 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import { FormList } from '@/presentation/pages'
 
+const makeSut = (): void => {
+  render(<FormList />)
+}
+
 describe('FormList Component', () => {
   test('should present skeleton on start', () => {
-    render(<FormList />)
+    makeSut()
     const table = screen.getByTestId('table-responsive')
     expect(table.childNodes.length).toBe(1)
     expect(table.querySelector('svg').getAttribute('role')).toBe('img')
