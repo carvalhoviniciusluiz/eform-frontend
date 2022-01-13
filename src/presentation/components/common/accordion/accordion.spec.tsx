ff1a809fcb6e6f5d01ccca7d87fc137d98394f50
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-01-13 09:49:49 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/common/accordion/accordion.spec.tsx
+++ src/presentation/components/common/accordion/accordion.spec.tsx
@@ -2,19 +2,20 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import * as faker from 'faker'
 import { Accordion } from '@/presentation/components'
 
+const makeSut = (isOpen = false): void => {
+  render(
+    <Accordion title={faker.random.word()} isOpen={isOpen}>
+      <div />
+    </Accordion>
+  )
+}
+
 describe('Accordion Component', () => {
   test('should open accordion', () => {
-    render(
-      <Accordion title={faker.random.word()} isOpen={false}>
-        <div />
-      </Accordion>
-    )
-
+    makeSut()
     const button = screen.getByTestId('accordion-button')
     const panel = screen.getByTestId('accordion-panel')
-
     fireEvent.click(button)
-
     expect(button).toHaveClass('active')
     expect(panel).toHaveClass('open')
   })
