fd4f75d88373c7f30f71cbc351eb2658adf6c213
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-01-17 16:15:21 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,8 @@
-import { AccountModel, UnexpectedError } from '@/domain'
+import { AccountModel } from '@/domain'
 import { LOCAL_STORAGE_KEY } from '@/main/config/constants'
 import { makeLocalStorageAdapter } from '@/main/factories'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
   makeLocalStorageAdapter().set(LOCAL_STORAGE_KEY, account)
 }
 
