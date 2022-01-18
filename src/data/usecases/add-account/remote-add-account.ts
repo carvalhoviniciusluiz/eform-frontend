69cec8a7730ce0e3a8cb2ee0254920fff701c75c
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-01-18 17:02:37 -0300

    feat: ensure RemoteAddAcount returns 201

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -19,6 +19,8 @@ export class RemoteAddAccount implements AddAccount {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
         return httpResponse.body?.data
+      case HttpStatusCode.created:
+        return httpResponse.body?.data
       case HttpStatusCode.forbidden:
         throw new EmailInUseError()
       default:
